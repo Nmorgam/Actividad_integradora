4c1afca6a1739fc3f9ff07b1e748617188b1d7a0
Bengibor <81210041+Nmorgam@users.noreply.github.com>
2024-11-26 23:06:22 -0600

    cargando carpeta con mi sitio

---
+++ Mi sitio/script.js
@@ -0,0 +1,5 @@
+// Agregar interacción simple
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("¡Gracias por tu mensaje!");
+});
